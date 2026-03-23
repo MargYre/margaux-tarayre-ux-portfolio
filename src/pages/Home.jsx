a4d647741b2e7591676d445b8ee5e3c82f5cb50b
Margaux <margaux.tarayre@gmail.com>
2026-03-23 15:29:16 +0100

    feat: curate homepage projects display to 3 selected Project.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
+import { useTranslation } from 'react-i18next'
 import HeroSection from '../components/Hero/HeroSection'
 import ProjectsGrid from '../components/Projects/ProjectsGrid'
 import Footer from '../components/Footer/Footer'
 import Button from '../components/Button/Button'
+import styles from './Home.module.scss'
 
 const Home = () => {
+  const { t } = useTranslation()
+
   return (
     <>
       <HeroSection />
-      <ProjectsGrid limit={6} />
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '2.5rem 0 3rem' }}>
-        <Button variant="primary" href="/projects">
-          See all projects
+      <ProjectsGrid limit={3} />
+      <div className={styles.viewAllWrapper}>
+        <Button variant="primary" href="/projects" className={styles.seeAllButton}>
+          {t('home.projects.seeAll')}
         </Button>
       </div>
       <Footer />
